faeb0e8c67ee1b2538164d12ed8a0dded95da1a1
david0x00 <null2@illinois.edu>
2020-07-18 00:17:09 -0500

    added PD control and filtering

--- serialControl/serialControl.ino
+++ serialControl/serialControl.ino
@@ -16,26 +16,28 @@ double ref_temp = 100;
 double P, I, D;
 double Kp = 1;
 double Ki = 0;
-double Kd = 0;
+double Kd = 10;
+
+double alpha = 0.1;
+
 double error = 0;
+double prev_error = 0;
+double integral = 0;
+double derivative = 0;
 
 double control;
 
 bool print_flag = false;
-
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(relay, OUTPUT);
-
-  while (!Serial) delay(1);
-  delay(500);
-}
+bool heater_state = false;
 
 void heatOn() {
   if (print_flag){
     Serial.println("heat On");
   }
+  if (heater_state == false) {
+    integral = 0;
+  }
+  heater_state = true;
   digitalWrite(relay, HIGH);
 }
 
@@ -43,6 +45,10 @@ void heatOff() {
   if (print_flag) {
     Serial.println("heat Off");
   }
+  if (heater_state == true) {
+    integral = 0;
+  }
+  heater_state = false;
   digitalWrite(relay, LOW);
 }
 
@@ -67,6 +73,15 @@ void printInfo() {
   Serial.print(curr_temp);
   Serial.print(", error: ");
   Serial.println(error);
+  
+  Serial.print("P: ");
+  Serial.print(P);
+  Serial.print(", I: ");
+  Serial.print(I);
+  Serial.print(", D: ");
+  Serial.print(D);
+  Serial.print(", control: ");
+  Serial.println(control);
 }
 
 void plotTemp() {
@@ -74,14 +89,32 @@ void plotTemp() {
   Serial.println(curr_temp);
 }
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+  P = 0;
+  I = 0;
+  D = 0;
+
+  while (!Serial) delay(1);
+  delay(500);
+  curr_temp = getTemperature();
+}
+
 void loop() {
   prev_temp = curr_temp;
-  curr_temp = getTemperature();
+  curr_temp = alpha*curr_temp + (1.0-alpha)*getTemperature();
+  prev_error = error;
+
   error = ref_temp - curr_temp;
+  integral += error;
+  derivative = error - prev_error;
 
   P = Kp * error;
+  I = Ki * integral;
+  D = Kd * derivative; 
 
-  control = P;
+  control = P + I + D;
 
   //printInfo();  
   plotTemp();
